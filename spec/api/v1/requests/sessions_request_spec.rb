14c5e791fa440f4449cc3c19020968a1882f5f6c
Caleb Wittman <calebjwittman1991@gmail.com>
2021-08-08 09:19:52 -0600

    Add additional testing

--- spec/api/v1/requests/sessions_request_spec.rb
+++ spec/api/v1/requests/sessions_request_spec.rb
@@ -2,10 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions API' do
   describe 'create' do
-    xit 'logs a user in' do
-      post '/api/v1/sessions'
+    before :each do
+      json_payload = {
+        email: 'Roald91',
+        password: 'IceCold',
+        password_confirmation: 'IceCold'
+        }
+
+      post '/api/v1/users', params: json_payload, as: :json
+    end
+
+    it 'logs a user in' do
+      post '/api/v1/sessions', params: {email: 'Roald91', password: 'IceCold'}, as: :json
 
       expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data][:attributes][:email]).to eq('Roald91')
+      expect(json[:data][:attributes][:api_key]).to eq(User.last.api_key)
+    end
+
+    it "doesn't log a user in if bad credentials" do
+      post '/api/v1/sessions', params: {email: 'Roald91', password: 'Iceold'}, as: :json
+
+      expect(response).to_not be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data][:attributes][:errors]).to eq("Your password or email is incorrect.")
     end
   end
 end
--- spec/api/v1/requests/user_request_spec.rb
+++ spec/api/v1/requests/user_request_spec.rb
@@ -1,22 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'User API' do
-  xit 'creates a user' do
+  it 'creates a user' do
     json_payload = {
-      username: 'Roald91',
+      email: 'Roald91',
       password: 'IceCold',
       password_confirmation: 'IceCold'
       }
 
-    headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
-
-    post '/api/v1/users', headers: headers, params: json_payload.to_json, as: :json
+    post '/api/v1/users', params: json_payload, as: :json
 
     expect(response).to be_successful
 
+    request_body = JSON.parse(request.body.read, symbolize_names: true)
+
+    expect(request_body[:email]).to eq('Roald91')
+    expect(request_body[:password]).to eq('IceCold')
+    expect(request_body[:password_confirmation]).to eq('IceCold')
+
     user = User.last
 
-    expect(user.username).to eq('Roald')
-    expect(user.password).to be_a(String)
+    expect(user.email).to eq('Roald91')
+    expect(user.password_digest).to be_a(String)
+    expect(user.api_key).to be_a(String)
   end
+
+  it 'can catch password errors' do
+    json_payload = {
+      email: 'Roald91',
+      password: 'IceCold',
+      password_confirmation: 'IceCod'
+      }
+
+    post '/api/v1/users', params: json_payload, as: :json
+
+    expect(response).to_not be_successful
+
+    error_message = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_message[:data][:attributes][:errors]).to eq(["Password confirmation doesn't match Password"])
+  end
+
 end
