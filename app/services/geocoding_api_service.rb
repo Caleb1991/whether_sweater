ad4d62cdbdb1143ca03e27691600f99fc3ac563d
Caleb Wittman <calebjwittman1991@gmail.com>
2021-08-10 07:25:24 -0600

    Refactor openweather service to have a conn class method

--- app/services/geocoding_api_service.rb
+++ app/services/geocoding_api_service.rb
@@ -1,6 +1,6 @@
 class GeocodingApiService
   def self.get_lat_and_long(city_state)
-    response = Faraday.get("http://www.mapquestapi.com/geocoding/v1/address") do |req|
+    response = conn.get("/geocoding/v1/address") do |req|
       req.params['key'] = ENV['geocoding_api_key']
       req.params['location'] = city_state
     end
@@ -9,12 +9,16 @@ class GeocodingApiService
   end
 
   def self.get_route_information(origin, destination)
-    response = Faraday.get('http://www.mapquestapi.com/directions/v2/route') do |req|
+    response = conn.get('/directions/v2/route') do |req|
       req.params['key'] = ENV['geocoding_api_key']
       req.params['from'] = origin
       req.params['to'] = destination
     end
 
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com')
   end
 end
